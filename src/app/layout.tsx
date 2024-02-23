1969180f6765f4c24b24eac75903db1d36d33a7d
Kelvin Ward <kelvinward1010@gmail.com>
2024-02-23 17:42:43 +0700

    update navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Navbar } from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="light" suppressContentEditableWarning>
       <body className={cn(
-        'min-h-screen font-sans antialiased',
+        'min-h-screen font-sans antialiased grainy',
         inter.className
       )}>
+        <Navbar />
         {children}
       </body>
     </html>
